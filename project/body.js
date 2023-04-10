9b25ad70fb3e012e52ee52ccf223ca80466a7295
arceeluceno13 <lucenoarcee0@gmail.com>
2023-04-10 22:41:55 +0800

    animation/footer/body

--- project/body.js
+++ project/body.js
@@ -1,7 +1,7 @@
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
   
-    for (var i = 0; i < reveals.length; i++) {
+    for (var i = 0; i < reveals.length; i++ )  {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
       var elementVisible = 150;
